7ba37d95871c99f2ff14c3ccb9b9e2c6ff010261
ajzal-byte <ajzalaju10@gmail.com>
2024-04-07 15:11:48 +0530

    tard form styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { Link, Route, Routes } from "react-router-dom";
 import { TraditionalForm } from "./components";
+import "./App.css"
 
 const App = () => {
-  
   return (
     <>
-      <TraditionalForm />
+      <div className="links">
+      <Link to="/traditional">Traditional Form</Link>
+      <Link to="/traditional">React Hook Form</Link>
+      <Link to="/traditional">React Hook Form with Zod</Link>
+      </div>
+
+      <Routes>
+        <Route path="/traditional" element={<TraditionalForm />} />
+      </Routes>
     </>
   );
 };
